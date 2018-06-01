a80e13b7dbebdf42461ee6c1faa02078fd865c4f
Peter Kim <peterckim96@gmail.com>
2018-06-01 18:25:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
   when 2
     array.join(" and ")
   when 3
-    array.join(", ")
+    array[0..1].join(", ")
+    array[2].join(", and ")
   else
     array.join(", ")
   end
