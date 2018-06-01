b11153365c921835694ebdb868ab8a452f5492e4
Peter Kim <peterckim96@gmail.com>
2018-06-01 18:30:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,6 @@ def oxford_comma(array)
     array[0]
   when 2
     array.join(" and ")
-  when 3
-    array[-1] = "and #{array[-1]}"
-    array.join(", ")
   else
     array[-1] = "and #{array[-1]}"
     array.join(", ")
